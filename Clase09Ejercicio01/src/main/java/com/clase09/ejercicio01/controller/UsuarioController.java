208b839ac8a903fa2db49475ed97baaea07e608f
Caro Bima <carobima@gmail.com>
2024-01-19 06:11:26 -0300

    Cargando resolución del ejercicio 1 de la clase 9

--- Clase09Ejercicio01/src/main/java/com/clase09/ejercicio01/controller/UsuarioController.java
+++ Clase09Ejercicio01/src/main/java/com/clase09/ejercicio01/controller/UsuarioController.java
@@ -3,27 +3,39 @@ package com.clase09.ejercicio01.controller;
 import com.clase09.ejercicio01.entities.Usuario;
 import com.clase09.ejercicio01.services.IUsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.LinkedList;
 import java.util.List;
 
+//@RestController
 @Controller
 public class UsuarioController {
 
     @Autowired
     IUsuarioService usuarioService;
 
+    @Value("${titulo}")
+    private String titulo;
+
     @GetMapping("/usuarios")
     public String getListaUsuarios(Model model){
         List<Usuario> listaUsuarios = usuarioService.traerTodosLosUsuarios();
 
-        model.addAttribute("titulo", "Listado de Usuarios");
+        model.addAttribute("titulo", titulo);
 
         model.addAttribute("listaUsuarios", listaUsuarios);
 
         return "index";
+    }
+
+    @GetMapping("/usuariosrest")
+    public List<Usuario> getListaUsuarios(){
+       return  usuarioService.traerTodosLosUsuarios();
+
     }
 }
