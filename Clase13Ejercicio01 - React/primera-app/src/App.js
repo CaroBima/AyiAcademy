0112dd606c1e1721907ff586e5248d1611aa4337
CaroBima <carobima@gmail.com>
2024-01-24 19:55:28 -0300

    agregando resolución de ejercicio 3

--- Clase13Ejercicio01 - React/primera-app/src/App.js
+++ Clase13Ejercicio01 - React/primera-app/src/App.js
@@ -14,6 +14,40 @@ function App() {
         <br/>
         <input type="submit" value="Ingresar"/>
       </form>
+      <RegistrarUsuario/>
+    </div>
+  );
+}
+
+function RegistrarUsuario(){
+
+return (
+    <div className="registrarUsuario">
+      <h1>Registro de usuario</h1>
+      <form>
+        <label for="nombre">Nombre: </label>
+        <input type="text" id="nombre" name="nombre"/>
+        <br/>
+        <label for="apellido">Apellido: </label>
+        <input type="text" id="apellido" name="apellido"/>
+        <br/>
+        <label for="tipoDni">Tipo de documento: </label>
+        <select name="tipoDni" id="tipoDni">
+          <option value="value1">Dni</option>
+          <option value="value1">Pasaporte</option>
+        </select>
+        <br/>
+        <label for="nroDni">Nro de documento: </label>
+        <input type="text" id="nroDni" name="nroDni"/>
+        <br/>
+        <label for="domicilio">Domicilio: </label>
+        <input type="text" id="domicilio" name="domicilio"/>
+        <br/>
+        <label for="telefono">Teléfono: </label>
+        <input type="text" id="telefono" name="telefono"/>
+        <br/>
+        <input type="submit" value="Ingresar"/>
+      </form>
     </div>
   );
 }
