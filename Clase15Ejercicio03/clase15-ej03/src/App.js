7944fca7db715dd15562d6f3982c59839f6b04dd
Caro Bima <carobima@gmail.com>
2024-01-28 08:06:49 -0300

    Agregada pantalla con input para ingresar el nombre y handles para el input y cambio de color

---
+++ Clase15Ejercicio03/clase15-ej03/src/App.js
@@ -0,0 +1,43 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/js/bootstrap.min.js";
+import {createRef, useState} from 'react';
+import './App.css';
+
+
+/*
+3) Crear un proyecto de React desde cero:
+Editar la vista que viene por default
+Crear un formulario que solicite nombre y un botón de enviar, al presionarlo se abrirá un nuevo 
+componente con el mensaje de bienvenida
+El nuevo componente debe tener un boton que al presionarlo, cambiara de color el mensaje de bienvenida.
+*/
+
+function App() {
+
+  const [nombre, setNombre] = useState('');
+
+  const handleInputChange = (event) => {
+    setNombre(event.target.value);
+  };
+
+  const handleEnviarClick = () => {
+    console.log("manda el click " + nombre);
+    //aca me falta activar el componente hijo
+      };
+  
+
+  return (
+    <div className="row justify-content-md-center mt-5">
+      <div className="col-md-6 text-center mt-5">
+        <div className="form-floating mb-3">
+          <input type="text" className="form-control" id="nombreInput" onChange={handleInputChange}/>
+          <label htmlFor="nombreInput">Nombre</label>
+        </div>
+        <button className="btn btn-outline-success btn-lg col-md-6" type="submit" onClick={handleEnviarClick}>Enviar</button>
+      </div>
+    </div>
+  );
+}
+
+
+export default App;
