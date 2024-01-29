5c0de24dbc040610d133db5f40552f3e6daa8612
CaroBima <carobima@gmail.com>
2024-01-29 17:09:58 -0300

    Comenzando ejercicio 04 de la clase 15

---
+++ Clase15Ejercicio04/clase15ej04/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+/*
+Crear un proyecto de React desde cero:
+Editar la vista que viene por default
+Crear un botón que al presionarlo renderize otro 
+componente y muestre datos de personas, los datos se deben extraer de un json
+*/
+function App() {
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+
+export default App;
